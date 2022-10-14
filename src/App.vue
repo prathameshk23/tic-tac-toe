8df4ee549b64aafac4185492425daf266ece5d10
prathamesh <prathameshkulkarni952@gmail.com>
2022-10-14 23:10:05 +0530

    init

--- src/App.vue
+++ src/App.vue
@@ -1,62 +1,95 @@
 <script setup>
-import { computed } from '@vue/reactivity';
-import { ref } from 'vue';
+import { ref, computed } from "vue";
 
-const player = ref('X');
+const player = ref("X");
 const board = ref([
-  ['','',''],
-  ['','',''],
-  ['','',''],
-])
+  ["", "", ""],
+  ["", "", ""],
+  ["", "", ""],
+]);
 
- const calculateWinner = (squares) => {
+const CalculateWinner = (board) => {
   const lines = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6],
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
   ];
+
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
+
+    if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+      return board[a];
     }
   }
+
   return null;
-}
+};
 
-const winner = computed(() => calculateWinner(board.value.flat()))
+const winner = computed(() => CalculateWinner(board.value.flat()));
 
-const move = (x,y) => {
-  if (winner.value) return
-  if (board.value[x][y] !== '') return
+const MakeMove = (x, y) => {
+  if (winner.value) return;
 
-  board.value[x][y] = player.value
+  if (board.value[x][y]) return;
 
-  player.value = player.value === 'X' ? 'O' : 'X'
-}
-  
-const reset = () => {
-    board.values = [
-    ['','',''],
-    ['','',''],
-    ['','',''],
-  ]
-  player.value ='X'
-  
-}
+  board.value[x][y] = player.value;
+
+  player.value = player.value === "X" ? "O" : "X";
+};
+
+const ResetGame = () => {
+  board.value = [
+    ["", "", ""],
+    ["", "", ""],
+    ["", "", ""],
+  ];
+  player.value = "X";
+};
 </script>
 
 <template>
   <main class="pt-8 text-center">
-    <h1 mb-8 text-3x1 font-bold uppercase>Tic Tac Toe</h1>
+    <h1 class="mb-8 text-3xl font-bold uppercase">Tic Tac Toe</h1>
+
+    <h3 class="text-xl mb-4">Player {{ player }}'s turn</h3>
+
+    <div class="flex flex-col items-center mb-8">
+      <div v-for="(row, x) in board" :key="x" class="flex">
+        <div
+          v-for="(cell, y) in row"
+          :key="y"
+          @click="MakeMove(x, y)"
+          :class="`border border-white w-24 h-24 hover:bg-gray-700 flex items-center justify-center material-icons-outlined text-4xl cursor-pointer ${
+            cell === 'X' ? 'text-gray-900' : 'text-blue-900'
+          }`"
+        >
+          {{ cell === "X" ? "close" : cell === "O" ? "circle" : "" }}
+        </div>
+      </div>
+    </div>
+
+    <div class="text-center">
+      <h2 v-if="winner" class="text-6xl font-bold mb-8">
+        Player '{{ winner }}' wins!
+      </h2>
+      <button
+        @click="ResetGame"
+        class="px-4 py-2 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-white rounded uppercase font-bold hover:bg-pink-600 duration-300"
+      >
+        Reset
+      </button>
+    </div>
   </main>
 </template>
 
-<style scoped>
-
+<style>
+body {
+  @apply bg-gradient-to-r from-cyan-300 to-blue-500 text-purple-600;
+}
 </style>
