6ad9b461bba0d090ec0de49a1687a915644c0536
prathamesh <prathameshkulkarni952@gmail.com>
2022-10-14 22:29:20 +0530

    js

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,62 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { computed } from '@vue/reactivity';
+import { ref } from 'vue';
+
+const player = ref('X');
+const board = ref([
+  ['','',''],
+  ['','',''],
+  ['','',''],
+])
+
+ const calculateWinner = (squares) => {
+  const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
+const winner = computed(() => calculateWinner(board.value.flat()))
+
+const move = (x,y) => {
+  if (winner.value) return
+  if (board.value[x][y] !== '') return
+
+  board.value[x][y] = player.value
+
+  player.value = player.value === 'X' ? 'O' : 'X'
+}
+  
+const reset = () => {
+    board.values = [
+    ['','',''],
+    ['','',''],
+    ['','',''],
+  ]
+  player.value ='X'
+  
+}
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
+  <main class="pt-8 text-center">
+    <h1 mb-8 text-3x1 font-bold uppercase>Tic Tac Toe</h1>
   </main>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './main.css'
 
-import './assets/main.css'
+// import './assets/main.css'
 
 createApp(App).mount('#app')
